fe9cc416f0d22d666d7ed2826b28f73f22476471
= <=>
2024-01-19 07:23:45 -0300

    Views updated

--- src/components/projects/ProjectsSlider.jsx
+++ src/components/projects/ProjectsSlider.jsx
@@ -9,13 +9,13 @@ export const ProjectsSlider = () => {
   return (
     <>
       <div className="grid grid-cols-2 gap-5">
-        {state.projects.map((project) => (
+        {state.projects?.map((project) => (
           <div className="flex flex-col h-80 mb-5" key={project.project_id}>
             <h2 className="text-center text-xl font-semibold text-slate-50 mb-2">
               {project.project_name}
             </h2>
             <Carousel className="dark">
-              {project.Images.map((image) => (
+              {project.Images?.map((image) => (
                 <img src={image.image_url} alt={image.image_alt} key={image.image_id} className="h-full"/>
               ))}
             </Carousel>
@@ -26,13 +26,13 @@ export const ProjectsSlider = () => {
             <div className="text-center gap-5 mt-3">
               <a
                 className="text-slate-100 hover:bg-gray-300 hover:text-slate-800 p-2 rounded-lg"
-                href={project.Link.link_visit_url} target="_blank"
+                href={project.Link?.link_visit_url} target="_blank"
               >
                 Visit site
               </a>
               <a
                 className="text-slate-100 hover:bg-gray-300 hover:text-slate-800 p-2 rounded-lg"
-                href={project.Link.link_code_url} target="_blank"
+                href={project.Link?.link_code_url} target="_blank"
               >
                 Github repository
               </a>
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,7 +1,7 @@
 
 export const Home = () => {
   return (
-      <div className="w-full flex flex-col md:flex-row items-center justify-around animate__animated animate__fadeIn">
+      <div className="w-full min-h-96 flex flex-col md:flex-row items-center justify-around animate__animated animate__fadeIn">
         <div className="md:w-2/4 mb-10 md:mb-0  ">
           <h1 className="text-2xl md:text-7xl text-slate-200 text-classic ">
             Hi, I'm Juan Pablo Jacobi, web developer
