d87c50b002c1abb06ffa8e73d724957f4dcd610e
= <=>
2024-01-26 01:36:41 -0300

    updated

--- src/components/projects/ProjectsSlider.jsx
+++ src/components/projects/ProjectsSlider.jsx
@@ -9,7 +9,7 @@ export const ProjectsSlider = () => {
     <>
       <div className="grid lg:grid-cols-2 gap-5">
         {state.projects?.map((project) => (
-          <div className="flex flex-col h-80 w-full m-auto" key={project.project_id}>
+          <div className="flex flex-col h-80 lg:w-full w-3/4 m-auto" key={project.project_id}>
             <h2 className="text-center text-2xl font-semibold text-slate-50 mb-2">
               {project.project_name}
             </h2>
