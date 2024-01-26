78bd25ebc86dba35de90af6153bf4c5214de9b08
= <=>
2024-01-25 21:02:11 -0300

    watsapp link added

--- src/components/shared/social-media-icons/SocialMedia.jsx
+++ src/components/shared/social-media-icons/SocialMedia.jsx
@@ -27,7 +27,11 @@ export const SocialMedia = () => {
         <a href='mailto:juanpjacobi@gmail.com' className="bg-red-500 p-2 font-semibold text-white inline-flex items-center space-x-2 rounded">
           <svg xmlns="http://www.w3.org/2000/svg"  aria-hidden="true" role="img" className="w-5" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><g fill="none"><path d="M12 0C5.372 0 0 5.373 0 12s5.372 12 12 12c6.627 0 12-5.373 12-12S18.627 0 12 0zm.14 19.018c-3.868 0-7-3.14-7-7.018c0-3.878 3.132-7.018 7-7.018c1.89 0 3.47.697 4.682 1.829l-1.974 1.978v-.004c-.735-.702-1.667-1.062-2.708-1.062c-2.31 0-4.187 1.956-4.187 4.273c0 2.315 1.877 4.277 4.187 4.277c2.096 0 3.522-1.202 3.816-2.852H12.14v-2.737h6.585c.088.47.135.96.135 1.474c0 4.01-2.677 6.86-6.72 6.86z" fill="currentColor"/></g></svg>
         </a>
-      </div>
+    <a href='https://api.whatsapp.com/send?phone=3513033252' target='_blank'  className="bg-green-500 p-2 font-semibold text-white inline-flex items-center space-x-2 rounded">
+    <svg xmlns="http://www.w3.org/2000/svg" className='w-5' viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"> <path stroke="none" d="M0 0h24v24H0z" fill="none"/> <path d="M3 21l1.65 -3.8a9 9 0 1 1 3.4 2.9l-5.05 .9" /> <path d="M9 10a0.5 .5 0 0 0 1 0v-1a0.5 .5 0 0 0 -1 0v1a5 5 0 0 0 5 5h1a0.5 .5 0 0 0 0 -1h-1a0.5 .5 0 0 0 0 1" /> </svg>
+
+    </a>
+  </div>
     </div>
   </div>
   )
