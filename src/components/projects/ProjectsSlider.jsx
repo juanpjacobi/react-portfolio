e32f3c67ec46e3dd37707a656da09f00d00deaed
= <=>
2024-01-26 21:27:07 -0300

    updated

--- src/components/projects/ProjectsSlider.jsx
+++ src/components/projects/ProjectsSlider.jsx
@@ -7,9 +7,9 @@ export const ProjectsSlider = () => {
 
   return (
     <>
-      <div className="grid lg:grid-cols-2 gap-5">
+      <div className="grid sm:grid-cols-2 gap-5">
         {state.projects?.map((project) => (
-          <div className="flex flex-col h-80 md:w-full w-5/6 m-auto" key={project.project_id}>
+          <div className="flex flex-col h-80 w-full m-auto" key={project.project_id}>
             <h2 className="text-center text-2xl font-semibold text-slate-50 mb-2">
               {project.project_name}
             </h2>
@@ -25,11 +25,11 @@ export const ProjectsSlider = () => {
             </Carousel>
             <div className="text-center mt-2">
               {project.project_description.split(" ").map((e, index) => (
-                  <span key={index} className="inline-block items-center me-2 mb-2 capitalize py-0.5 px-1.5 md:px-2.5 rounded-full text-xs md:text-base border border-slate-100 text-slate-100">{e}</span>
+                  <span key={index} className="inline-block items-center me-2 mb-2 capitalize py-0.5 px-1.5 lg:px-2.5 rounded-full text-xs lg:text-base border border-slate-100 text-slate-100">{e}</span>
                   ))}
             </div>
 
-            <div className="text-center mt-1 text-xs md:text-base">
+            <div className="text-center mt-1 text-xs lg:text-base">
               <a
                 className="text-slate-100 hover:bg-gray-300 hover:text-slate-800 p-2 rounded-lg"
                 href={project.Link?.link_visit_url}
