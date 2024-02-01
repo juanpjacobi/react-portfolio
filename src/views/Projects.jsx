d6ab2f29cb4c28732650da3308ceb4dad4d4ae47
= <=>
2024-01-31 22:16:15 -0300

    portfolio links added

--- src/views/Projects.jsx
+++ src/views/Projects.jsx
@@ -9,6 +9,16 @@ export const Projects = () => {
         My projects
       </h1>
       <ProjectsSlider/>
+      <div className="mt-5 text-center flex flex-col">
+
+      <p className=" text-slate-200 underline underline-offset-2 text-classic">Check the code of this portfolio here:</p>
+      <div className="flex row text-center m-auto gap-5 mt-2 text-sm md:text-base">
+      <span className="text-slate-100 hover:bg-gray-300 hover:text-slate-800 p-1 rounded-lg"><a href="https://github.com/juanpjacobi/react-portfolio-backend" target="_blank">Backend</a></span>
+      <span className="text-slate-100 hover:bg-gray-300 hover:text-slate-800 p-1 rounded-lg"><a href="https://github.com/juanpjacobi/react-portfolio" target="_blank">React app</a></span>
+
+      </div>
+      </div>
+
     </div>
   );
 };
