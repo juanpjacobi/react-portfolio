c1468cb03a69f01291db19f14fb4e05b5c5f074d
= <=>
2024-08-12 19:47:56 -0300

    home updated

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -4,7 +4,7 @@ export const Home = () => {
       <div className="w-full min-h-96 flex flex-col md:flex-row items-center justify-around animate__animated animate__fadeIn">
         <div className="md:w-2/4 mb-10 md:mb-0  ">
           <p className=" md:text-5xl text-center text-slate-200 text-classic ">
-           Welcome! My name is Juan Pablo Jacobi and this is my portoflio where I share you my projects.
+           Welcome! My name is Juan Pablo Jacobi and this is my portoflio where I share my projects.
           </p>
         </div>
         <div className="md:w-1/4 w-2/4">
